7d8bcebaddad8fca52f401ca076b782c483801b3
wanghanbin <544430497@qq.com>
2026-04-01 17:29:51 +0800

    [更新] 更新配置

--- variants/win64-iu-single.sh
+++ variants/win64-iu-single.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 source "$(dirname "$BASH_SOURCE")"/windows-install-iu-single.sh
 source "$(dirname "$BASH_SOURCE")"/defaults-gpl.sh
-FF_CONFIGURE="--enable-nonfree $FF_CONFIGURE --disable-autodetect --disable-doc --disable-network --disable-ffplay --disable-ffprobe --disable-everything --enable-ffmpeg --enable-avcodec --enable-avfilter --enable-avformat --enable-avdevice --enable-swscale --enable-swresample --enable-protocol=file --enable-protocol=pipe --enable-indev=wasapi --enable-muxer=hls --enable-muxer=mpegts --enable-filter=gfxcapture --enable-filter=hwdownload --enable-filter=format --enable-encoder=libx264 --enable-encoder=h264_nvenc --enable-encoder=aac --enable-parser=h264 --enable-bsf=h264_mp4toannexb"
+FF_CONFIGURE="--enable-nonfree $FF_CONFIGURE \
+--disable-autodetect \
+--disable-doc \
+--disable-network \
+--disable-ffplay \
+--disable-ffprobe \
+--disable-iconv \
+--disable-opencl \
+--disable-vaapi \
+--disable-libvpl \
+--disable-liblcevc-dec \
+--disable-everything \
+--enable-ffmpeg \
+--enable-avcodec \
+--enable-avfilter \
+--enable-avformat \
+--enable-avdevice \
+--enable-swscale \
+--enable-swresample \
+--enable-protocol=file \
+--enable-protocol=pipe \
+--enable-indev=wasapi \
+--enable-muxer=hls \
+--enable-muxer=mpegts \
+--enable-muxer=mp4 \
+--enable-filter=gfxcapture \
+--enable-filter=hwdownload \
+--enable-filter=format \
+--enable-encoder=libx264 \
+--enable-encoder=h264_nvenc \
+--enable-encoder=aac \
+--enable-parser=h264 \
+--enable-bsf=h264_mp4toannexb"
 LICENSE_FILE=""
